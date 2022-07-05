97c74549127eba975d8b7b9d2679684fd06aa010
teffanlu <teffanypaola@gmail.com>
2022-07-05 11:39:15 -0400

    coneccion con base de datos de postgreSQL

--- src/utils/dbconnection.js
+++ src/utils/dbconnection.js
@@ -3,10 +3,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "docor",
-    password: "machupichu",
+    user: "dctypdogoqyrmi",
+    host: "ec2-23-23-151-191.compute-1.amazonaws.com",
+    database: "d9j3a5oj9o79pp",
+    password: "6b96a5b5dd00c291b945ad88f70e71f20bf79723b8a3c3bded2eda5e684937bc",
     port: 5432,
     /*connectionString: process.env.DBURI,
     max: 20,
